30e39a4967608214756e40c4ca46925758ddb191
CamiloTD <camilotd1999@gmail.com>
2018-07-25 05:43:39 -0500

    Test Server Added

--- test/app.js
+++ test/app.js
@@ -1,1 +1,15 @@
-let Coraline = require('Coraline');
+const http = require('http');
+const express = require('express');
+const socket_io = require('socket.io');
+const Coraline = require('coraline');
+const opn = require('opn');
+
+let app = express();
+let server = http.Server(app);
+let io = socket_io(app);
+
+app.use(express.static('public'));
+server.listen(8080, () => {
+	console.log("Server is listening at port :8080");
+	opn('http://localhost:8080');
+});
